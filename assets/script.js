f51ae9744237fc066836600a39a089a74364c6ad
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-05-31 19:04:13 -0300

    Upload of file

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const searchInput = document.querySelector('#shearch__input')
 
-searchInput.addEventListener('keydown', function (event) {
+searchInput.addEventListener('keyboard', function (event) {
   if (event.code === 'Enter') {
     search()
   }
@@ -15,4 +15,8 @@ function search() {
     '&hl=pt-BR&sxsrf=ALiCzsYAZMBAimAwR8lh4tt5mBF_HmALiA%3A1654011953849&source=hp&ei=MTiWYqX7MbmR1sQPo5i7kAo&iflsig=AJiK0e8AAAAAYpZGQWK1lTug1-rZyk7Xbg8RYmUkGnfS&gs_ssp=eJzj4tVP1zc0TCupKDPOyjUwYPRiS82rTM9NBABUjQdD&oq=' +
     input +
     '&gs_lcp=Cgdnd3Mtd2l6EAMYADILCC4QgAQQsQMQgwEyCwgAEIAEELEDEIMBMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQguEIAEMgUIABCABDIFCC4QgAQ6DQguEMcBEKMCEOoCECc6BwgjEOoCECc6CgguEMcBEKMCECc6BAgjECc6BAgAEEM6CAguELEDEIMBOggIABCxAxCDAToOCC4QsQMQgwEQxwEQowI6DQguEMcBEKMCENQCEEM6BwguELEDEEM6CAguEIAEELEDUIcnWJ5CYI9NaAJwAHgAgAGhAYgBpguSAQQwLjEwmAEAoAEBsAEK&sclient=gws-wiz'
+}
+
+function pesquisarEvento() {
+  search()
 }
